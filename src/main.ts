c0d90b93b2f7f6569e4c873d8cc70d1f2fc54675
datnt-zinza <121885719+datnt-zinza@users.noreply.github.com>
2023-02-03 09:25:04 +0700

    fix: test cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
-  app.enableCors();
+  app.enableCors({
+    // origin: [
+    //   'https://datnt1-react-ewq3mo8pl-datnt1-react.vercel.app/',
+    //   'http://localhost:3000',
+    // ],
+    // credentials: true,
+    origin: true,
+  });
   await app.listen(5000);
 }
 bootstrap();
